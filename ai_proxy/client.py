17036f7ad42188b7c0d99f8ee73493be5e626102
shilei.1108 <shilei.1108@shilei1108deMacBook-Pro.local>
2023-08-03 17:53:57 +0800

    shilei, modify code

--- ai_proxy/client.py
+++ ai_proxy/client.py
@@ -2,8 +2,9 @@ import logging
 import thriftpy2
 from thriftpy2.rpc import make_client
 ai_proxy_thrift = thriftpy2.load("ai_proxy/idl/ai_proxy.thrift", module_name="ai_proxy_thrift")
+base_thrift = thriftpy2.load("ai_proxy/idl/base.thrift", module_name="ai_proxy_thrift")
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -15,14 +16,37 @@ if __name__ == '__main__':
     print(client.Ping())
 
     chat_request = ai_proxy_thrift.ChatCompletionRequest(
-        model="gpt-35-turbo",
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "can you tell me who are you and who's your father"}
-        ]
+        ],
+        temperature=0.001,
+        Base=ai_proxy_thrift.base.Base(Caller="shilei")
     )
     chat_res = client.ChatCompletionCreate(chat_request)
-    print(f"chat_res: {chat_res}")
 
+    print(f"chat_request: {chat_request}\n")
+    print(f"chat_res: {chat_res}\n\n")
 
-    
+    tabledesc = """
+        # Employee-雇员(id, name, department_id, local_city-居住城市, city-办公城市, birth_date, gender, hire_date-入职日期)
+        # Department-部门(id, name, address-办公地址, tel-联系电话)
+        # Salary_Payments-薪酬支出(id, employee_id, amount, tax, date)
+        # Sales-销售(id, employee_id, product_id, state-状态 0:进行中、1:完成、2:取消, amount-销量, date)
+        # Product-产品(id, name, price, category)
+        """
+
+    text = "列出办公地址在北京的月平均薪酬最高的5个雇员及其销售量最大的产品名称"
+
+    langchain_request = ai_proxy_thrift.LangChainRequest(
+        model="gpt-3.5-turbo",
+        task="text-to-sql",
+        query=text,
+        context = {"tabledesc": tabledesc},
+        Base=ai_proxy_thrift.base.Base(Caller="shilei")
+    )
+    langchain_res = client.LangChainCreate(langchain_request)
+
+    print(f"langchain_request: {langchain_request}\n")
+    print(f"langchain_res: {langchain_res}\n\n")
--- ai_proxy/server.py
+++ ai_proxy/server.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import logging
 import json
 import multiprocessing
@@ -9,41 +11,22 @@ from thriftpy2.rpc import make_server
 from utils.langchain_util import LangChainClient
 from utils.openai_util import get_completion
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-ai_proxy_thrift = thriftpy2.load("ai_proxy/idl/ai_proxy.thrift", module_name="ai_proxy_thrift")
+file_path = os.path.abspath(os.path.dirname(__file__))
+logger.info(f"file_path {file_path}")
+root_path = os.path.abspath(os.path.join(file_path, '../'))
+logger.info(f"root_path {root_path}")
+sys.path.insert(0, root_path)
 
+ai_proxy_thrift = thriftpy2.load(f"{root_path}/ai_proxy/idl/ai_proxy.thrift", module_name="ai_proxy_thrift")
 
 class Handler(object):
 
     def Ping(self):
         return "Pong"
-
-    def ChatCompletionCreate(self, req):
-        model = req.model
-        messages = req.messages
-        temperature = req.temperature
-
-        model="gpt-35-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "can you tell me who are you and who's your father"}
-        ]
-        temperature=0.01
-        output = get_completion(messages=messages, model=model, temperature=temperature)
-        resp = ai_proxy_thrift.ChatCompletionResponse(
-            id=-1,
-            object=output,
-            json={},
-            BaseResp=ai_proxy_thrift.BaseResp(
-                StatusMessage=f"Success",
-                StatusCode=0
-            )
-        )
-        return resp
-
 
     def LangChainCreate(self, req):
         model_name = req.model
@@ -52,6 +35,11 @@ class Handler(object):
         context = req.context
         temperature = req.temperature
 
+        logger.info(f"model_name {model_name}")
+        logger.info(f"task {task}")
+        logger.info(f"query {query}")
+        logger.info(f"context {context}")
+        logger.info(f"temperature {temperature}")
 
         client = LangChainClient(model_name=model_name, temperature=temperature)
         if task == "text-to-sql":
@@ -67,22 +55,52 @@ class Handler(object):
             output = client.text_to_sql(query, tabledesc)
 
             resp = ai_proxy_thrift.LangChainResponse(
-                id=-1,
+                id='-1',
                 object=output,
-                json={},
-                BaseResp=ai_proxy_thrift.BaseResp(
+                json=json.dumps({}),
+                BaseResp=ai_proxy_thrift.base.BaseResp(
                     StatusMessage=f"Success",
                     StatusCode=0
                 )
             )
         else:
             resp = ai_proxy_thrift.LangChainResponse(
-                BaseResp=ai_proxy_thrift.BaseResp(
+                BaseResp=ai_proxy_thrift.base.BaseResp(
                     StatusMessage=f"not support {task} task",
                     StatusCode=1
                 )
             )
 
+        return resp
+
+    def ChatCompletionCreate(self, req):
+        logger.info(f"req {req}")
+
+        model = req.model
+        messages = req.messages
+        temperature = req.temperature
+
+        logger.info(f"model {model}")
+        logger.info(f"messages {messages}")
+        logger.info(f"temperature {temperature}")
+
+        # model="gpt-35-turbo",
+        # messages=[
+        #     {"role": "system", "content": "You are a helpful assistant."},
+        #     {"role": "user", "content": "can you tell me who are you and who's your father"}
+        # ]
+        # temperature=0.01
+
+        output = get_completion(messages=messages, model=model, temperature=temperature)
+        resp = ai_proxy_thrift.ChatCompletionResponse(
+            id='-1',
+            object=output,
+            json=json.dumps({}),
+            BaseResp=ai_proxy_thrift.base.BaseResp(
+                StatusMessage=f"Success",
+                StatusCode=0
+            )
+        )
         return resp
 
 
@@ -96,3 +114,19 @@ if __name__ == '__main__':
     server.serve()
 
     logger.info(f"start serving")
+
+
+    # handle = Handler()
+    #
+    # chat_request = ai_proxy_thrift.ChatCompletionRequest(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "system", "content": "You are a helpful assistant."},
+    #         {"role": "user", "content": "can you tell me who are you and who's your father"}
+    #     ],
+    #     temperature=0.001,
+    #     Base=ai_proxy_thrift.base.Base(Caller="shilei")
+    # )
+    #
+    # output = handle.ChatCompletionCreate(chat_request)
+    # print(output)
